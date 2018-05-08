db46178a89d04e9599d643b55eb7f63cd5c0c199
Akayeshmantha <kaperera@mitrai.com>
2018-05-08 09:58:09 +0530

    adding changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    production: false,
+
+    region: 'eu-west-1',
+
+    identityPoolId: 'eu-west-1:ca3c4d4f-f831-4318-9887-ec959ca05d74',
+    userPoolId: 'eu-west-1_HTb3dMJrQ',
+    clientId: '6m6a02juvpem8go6jm7rveggg3',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-1',
+
+    ddbTableName: 'LoginTrail',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+
+};
+
